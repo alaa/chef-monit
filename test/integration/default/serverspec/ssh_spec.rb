d9f03ee5b450b8c1d6314360ca3b02292e131e4a
Alaa Qutaish <alaa.qutaish@wimdu.com>
2014-12-16 12:59:10 +0100

    add ssh monit specs

---
+++ test/integration/default/serverspec/ssh_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "chef_monit::ssh" do
+
+  describe file("/etc/monit/conf.d/ssh") do
+    it { should be_file }
+    it { should be_mode 644 }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    its(:content) { should match(/group ssh/) }
+    its(:content) { should match(/totalmem/) }
+  end
+
+end
+
