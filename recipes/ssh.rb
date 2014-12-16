7b87d5e0d3a007f1c65ac4bb3e0a1688b5c83124
Alaa Qutaish <alaa.qutaish@wimdu.com>
2014-12-16 12:58:13 +0100

    add group and memory_check to the resource

--- recipes/ssh.rb
+++ recipes/ssh.rb
@@ -5,5 +5,7 @@ chef_monit_service "ssh" do
   start_command "/usr/sbin/service ssh start"
   stop_command "/usr/sbin/service ssh stop"
   pid_file "/tmp/sshd.pid"
+  group_name "ssh"
+  memory_limit "100"
 end
 
--- resources/service.rb
+++ resources/service.rb
@@ -6,4 +6,6 @@ attribute :service_name, :kind_of => String, :name_attribute => true
 attribute :pid_file, :kind_of => String
 attribute :start_command, :kind_of => String
 attribute :stop_command, :kind_of => String
+attribute :group_name, :kind_of => String
+attribute :memory_limit, :kind_of => String
 
